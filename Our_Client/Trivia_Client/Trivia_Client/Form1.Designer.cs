eed0de494dee4670f5719627c661c860f03be630
_TheLostFreak_ <idank7021@gmail.com>
2018-05-29 20:49:27 +0300

    Added new Client [our-client], Finished logIn screen (GUI)

---
+++ Our_Client/Trivia_Client/Trivia_Client/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Trivia_Client
+{
+    partial class LogInScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInScreen));
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.usernameWrap = new System.Windows.Forms.Panel();
+            this.passwordWrap = new System.Windows.Forms.Panel();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SignupBtn = new System.Windows.Forms.Button();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.exitBtn = new System.Windows.Forms.PictureBox();
+            this.usernameWrap.SuspendLayout();
+            this.passwordWrap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(65)))));
+            this.usernameBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameBox.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.usernameBox.Location = new System.Drawing.Point(9, 5);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(400, 26);
+            this.usernameBox.TabIndex = 0;
+            this.usernameBox.TabStop = false;
+            this.usernameBox.Text = "Username";
+            this.usernameBox.TextChanged += new System.EventHandler(this.usernameBox_TextChanged);
+            this.usernameBox.Enter += new System.EventHandler(this.usernameBox_Enter);
+            // 
+            // usernameWrap
+            // 
+            this.usernameWrap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(65)))));
+            this.usernameWrap.Controls.Add(this.usernameBox);
+            this.usernameWrap.Location = new System.Drawing.Point(259, 257);
+            this.usernameWrap.Name = "usernameWrap";
+            this.usernameWrap.Size = new System.Drawing.Size(420, 36);
+            this.usernameWrap.TabIndex = 1;
+            // 
+            // passwordWrap
+            // 
+            this.passwordWrap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(65)))));
+            this.passwordWrap.Controls.Add(this.passwordBox);
+            this.passwordWrap.Location = new System.Drawing.Point(259, 310);
+            this.passwordWrap.Name = "passwordWrap";
+            this.passwordWrap.Size = new System.Drawing.Size(420, 36);
+            this.passwordWrap.TabIndex = 2;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(65)))));
+            this.passwordBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordBox.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.passwordBox.Location = new System.Drawing.Point(9, 5);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(410, 26);
+            this.passwordBox.TabIndex = 0;
+            this.passwordBox.TabStop = false;
+            this.passwordBox.Text = "Password";
+            this.passwordBox.TextChanged += new System.EventHandler(this.passwordBox_TextChanged);
+            this.passwordBox.Enter += new System.EventHandler(this.passwordBox_Enter);
+            this.passwordBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.passwordBox_KeyDown);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.loginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginBtn.FlatAppearance.BorderSize = 0;
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginBtn.Font = new System.Drawing.Font("Open Sans Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.loginBtn.Location = new System.Drawing.Point(260, 361);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(200, 36);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.TabStop = false;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // SignupBtn
+            // 
+            this.SignupBtn.BackColor = System.Drawing.Color.Transparent;
+            this.SignupBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SignupBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.SignupBtn.FlatAppearance.BorderSize = 3;
+            this.SignupBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.SignupBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.SignupBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SignupBtn.Font = new System.Drawing.Font("Open Sans Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignupBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.SignupBtn.Location = new System.Drawing.Point(478, 361);
+            this.SignupBtn.Name = "SignupBtn";
+            this.SignupBtn.Size = new System.Drawing.Size(200, 36);
+            this.SignupBtn.TabIndex = 4;
+            this.SignupBtn.TabStop = false;
+            this.SignupBtn.Text = "Signup";
+            this.SignupBtn.UseVisualStyleBackColor = false;
+            // 
+            // logo
+            // 
+            this.logo.Image = global::Trivia_Client.Properties.Resources.logo;
+            this.logo.Location = new System.Drawing.Point(377, 180);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(165, 46);
+            this.logo.TabIndex = 6;
+            this.logo.TabStop = false;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exitBtn.BackgroundImage")));
+            this.exitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitBtn.Location = new System.Drawing.Point(877, 12);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(30, 30);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.TabStop = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            this.exitBtn.MouseLeave += new System.EventHandler(this.exitBtn_MouseLeave);
+            this.exitBtn.MouseHover += new System.EventHandler(this.exitBtn_MouseHover);
+            // 
+            // LogInScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(62)))), ((int)(((byte)(71)))));
+            this.ClientSize = new System.Drawing.Size(919, 596);
+            this.ControlBox = false;
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.SignupBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordWrap);
+            this.Controls.Add(this.usernameWrap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LogInScreen";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LogInScreen_Load);
+            this.usernameWrap.ResumeLayout(false);
+            this.usernameWrap.PerformLayout();
+            this.passwordWrap.ResumeLayout(false);
+            this.passwordWrap.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitBtn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Panel usernameWrap;
+        private System.Windows.Forms.Panel passwordWrap;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button SignupBtn;
+        private System.Windows.Forms.PictureBox exitBtn;
+        private System.Windows.Forms.PictureBox logo;
+    }
+}
+
---
+++ Our_Client/Trivia_Client/Trivia_Client/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Trivia_Client
+{
+    public partial class LogInScreen : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        public LogInScreen()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 6, 6));
+
+            usernameWrap.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, usernameWrap.Width, usernameWrap.Height, 5, 5));
+            passwordWrap.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, passwordWrap.Width, passwordWrap.Height, 5, 5));
+            loginBtn.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, loginBtn.Width, loginBtn.Height, 5, 5));
+            SignupBtn.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, SignupBtn.Width + 1, SignupBtn.Height + 1, 5, 5));
+            exitBtn.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, exitBtn.Width, exitBtn.Height, exitBtn.Height, exitBtn.Width));
+        }
+
+        private void usernameBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }        
+
+        private void passwordBox_TextChanged(object sender, EventArgs e)
+        {
+            passwordBox.PasswordChar = '•';
+        }
+
+        private void passwordBox_Enter(object sender, EventArgs e)
+        {
+            passwordBox.Text = "";
+            passwordBox.TabStop = true;
+            usernameBox.TabStop = true;
+        }
+
+        private void usernameBox_Enter(object sender, EventArgs e)
+        {
+            usernameBox.Text = "";
+            passwordBox.TabStop = true;
+            usernameBox.TabStop = true;
+        }
+
+        private void exitBtn_MouseHover(object sender, EventArgs e)
+        {
+            exitBtn.BackgroundImage = Trivia_Client.Properties.Resources.exitButtonHover;
+        }
+
+        private void exitBtn_MouseLeave(object sender, EventArgs e)
+        {
+            exitBtn.BackgroundImage = Trivia_Client.Properties.Resources.exitButton;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LogInScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13) // Enter
+            {
+                Application.Exit(); // In the future, it might be for log in.
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wparam, int lparam);
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Y <= 50)
+            {
+                Capture = false;
+                SendMessage(Handle, 0x00A1, 2, 0);
+            }
+        }
+    }
+}
