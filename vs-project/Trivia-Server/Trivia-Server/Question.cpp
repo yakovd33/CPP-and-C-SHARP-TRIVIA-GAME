0d0221737d4551a1654ddf233ca21fc75b3c0949
el_yashir <0hehr021@gmail.com>
2018-05-31 19:18:59 +0300

    qustion class (h+cpp)

--- vs-project/Trivia-Server/Trivia-Server/Question.cpp
+++ vs-project/Trivia-Server/Trivia-Server/Question.cpp
@@ -10,3 +10,23 @@ Question::Question()
 Question::~Question()
 {
 }
+
+string Question::getQuestion()
+{
+	return this->_question;
+}
+
+string * Question::getAnswers()
+{
+	return this->_answers;
+}
+
+int Question::getCorrectAnswersIndex()
+{
+	return this->_correctAnswersIndex;
+}
+
+int Question::getId()
+{
+	return this->_id;
+}
--- vs-project/Trivia-Server/Trivia-Server/Question.h
+++ vs-project/Trivia-Server/Trivia-Server/Question.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <iostream>
+
+using std::string;
+
+
 class Question {
 public:
 	Question();
 	~Question();
+
+	string getQuestion();
+	string* getAnswers();
+	int getCorrectAnswersIndex();
+	int getId();
+private:
+	string _question;
+	string _answers[4];
+	int _correctAnswersIndex;
+	int _id;
 };
 
