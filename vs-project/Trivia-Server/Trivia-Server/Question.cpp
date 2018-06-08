b67e961a4ec23524f45d07bbf20d5011978f74d2
yakovd33 <yakovd33@gmail.com>
2018-06-08 14:22:03 +0300

    [Server] Class question randomizer

--- vs-project/Trivia-Server/Trivia-Server/Question.cpp
+++ vs-project/Trivia-Server/Trivia-Server/Question.cpp
@@ -1,13 +1,23 @@
 #include "Question.h"
+#include <time.h>
 
 Question::Question(int id, string question, string correctAnswer, string answer2, string answer3, string answer4) {
+	srand(time(NULL));
 	this->_id = id;
 	this->_question = question;
-	this->_correctAnswersIndex = 0;
+	this->_correctAnswersIndex = rand() % 4;
 	this->_answers[0] = correctAnswer;
 	this->_answers[1] = answer2;
 	this->_answers[2] = answer3;
 	this->_answers[3] = answer4;
+
+	this->_answers[0] = this->_answers[_correctAnswersIndex];
+	this->_answers[_correctAnswersIndex] = correctAnswer;
+	std::cout << "Answer #1" << this->_answers[0] << std::endl;
+	std::cout << "Answer #2" << this->_answers[1] << std::endl;
+	std::cout << "Answer #3" << this->_answers[2] << std::endl;
+	std::cout << "Answer #4" << this->_answers[3] << std::endl;
+	std::cout << "correct" << this->_correctAnswersIndex << std::endl;
 }
 
 Question::~Question() {
