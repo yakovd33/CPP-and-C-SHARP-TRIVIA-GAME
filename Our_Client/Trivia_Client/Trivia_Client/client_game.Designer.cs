12b2e3c76b9901464cdbadb42b21cd673a550048
yakovd33 <yakovd33@gmail.com>
2018-06-15 18:37:15 +0300

    [client] new form named client game for the game, dont work properly (cant open it from signup, and need to contact with server to get Q&A).

---
+++ Our_Client/Trivia_Client/Trivia_Client/client_game.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Client_game
+{
+    partial class client_game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameWrap = new System.Windows.Forms.Panel();
+            this.Ans1_Btn = new System.Windows.Forms.Button();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.exitBtn = new System.Windows.Forms.PictureBox();
+            this.Ans4_Btn = new System.Windows.Forms.Button();
+            this.Ans3_Btn = new System.Windows.Forms.Button();
+            this.Ans2_Btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.room_info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitBtn)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // usernameWrap
+            // 
+            this.usernameWrap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(65)))));
+            this.usernameWrap.Location = new System.Drawing.Point(246, 121);
+            this.usernameWrap.Name = "usernameWrap";
+            this.usernameWrap.Size = new System.Drawing.Size(420, 36);
+            this.usernameWrap.TabIndex = 1;
+            // 
+            // Ans1_Btn
+            // 
+            this.Ans1_Btn.BackColor = System.Drawing.Color.Transparent;
+            this.Ans1_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Ans1_Btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans1_Btn.FlatAppearance.BorderSize = 3;
+            this.Ans1_Btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Ans1_Btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans1_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ans1_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans1_Btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.Ans1_Btn.Location = new System.Drawing.Point(466, 219);
+            this.Ans1_Btn.Name = "Ans1_Btn";
+            this.Ans1_Btn.Size = new System.Drawing.Size(200, 36);
+            this.Ans1_Btn.TabIndex = 4;
+            this.Ans1_Btn.TabStop = false;
+            this.Ans1_Btn.UseVisualStyleBackColor = false;
+            this.Ans1_Btn.Click += new System.EventHandler(this.SignupBtn_Click);
+            // 
+            // logo
+            // 
+            this.logo.Image = global::Trivia_Client.Properties.Resources.logo;
+            this.logo.Location = new System.Drawing.Point(31, 21);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(165, 46);
+            this.logo.TabIndex = 6;
+            this.logo.TabStop = false;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.exitBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitBtn.Location = new System.Drawing.Point(877, 12);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(30, 30);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.TabStop = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            this.exitBtn.MouseLeave += new System.EventHandler(this.exitBtn_MouseLeave);
+            this.exitBtn.MouseHover += new System.EventHandler(this.exitBtn_MouseHover);
+            // 
+            // Ans4_Btn
+            // 
+            this.Ans4_Btn.BackColor = System.Drawing.Color.Transparent;
+            this.Ans4_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Ans4_Btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans4_Btn.FlatAppearance.BorderSize = 3;
+            this.Ans4_Btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Ans4_Btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans4_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ans4_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans4_Btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.Ans4_Btn.Location = new System.Drawing.Point(466, 309);
+            this.Ans4_Btn.Name = "Ans4_Btn";
+            this.Ans4_Btn.Size = new System.Drawing.Size(200, 36);
+            this.Ans4_Btn.TabIndex = 7;
+            this.Ans4_Btn.TabStop = false;
+            this.Ans4_Btn.UseVisualStyleBackColor = false;
+            // 
+            // Ans3_Btn
+            // 
+            this.Ans3_Btn.BackColor = System.Drawing.Color.Transparent;
+            this.Ans3_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Ans3_Btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans3_Btn.FlatAppearance.BorderSize = 3;
+            this.Ans3_Btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Ans3_Btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans3_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ans3_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans3_Btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.Ans3_Btn.Location = new System.Drawing.Point(246, 309);
+            this.Ans3_Btn.Name = "Ans3_Btn";
+            this.Ans3_Btn.Size = new System.Drawing.Size(200, 36);
+            this.Ans3_Btn.TabIndex = 8;
+            this.Ans3_Btn.TabStop = false;
+            this.Ans3_Btn.UseVisualStyleBackColor = false;
+            // 
+            // Ans2_Btn
+            // 
+            this.Ans2_Btn.BackColor = System.Drawing.Color.Transparent;
+            this.Ans2_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Ans2_Btn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans2_Btn.FlatAppearance.BorderSize = 3;
+            this.Ans2_Btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Ans2_Btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(83)))), ((int)(((byte)(101)))));
+            this.Ans2_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ans2_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ans2_Btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(145)))), ((int)(((byte)(156)))));
+            this.Ans2_Btn.Location = new System.Drawing.Point(246, 219);
+            this.Ans2_Btn.Name = "Ans2_Btn";
+            this.Ans2_Btn.Size = new System.Drawing.Size(200, 36);
+            this.Ans2_Btn.TabIndex = 9;
+            this.Ans2_Btn.TabStop = false;
+            this.Ans2_Btn.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.room_info);
+            this.panel1.Location = new System.Drawing.Point(136, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(566, 345);
+            this.panel1.TabIndex = 10;
+            // 
+            // room_info
+            // 
+            this.room_info.AutoSize = true;
+            this.room_info.Location = new System.Drawing.Point(107, 76);
+            this.room_info.Name = "room_info";
+            this.room_info.Size = new System.Drawing.Size(0, 13);
+            this.room_info.TabIndex = 0;
+            // 
+            // client_game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(62)))), ((int)(((byte)(71)))));
+            this.ClientSize = new System.Drawing.Size(919, 596);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Ans2_Btn);
+            this.Controls.Add(this.Ans3_Btn);
+            this.Controls.Add(this.Ans4_Btn);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.Ans1_Btn);
+            this.Controls.Add(this.usernameWrap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "client_game";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LogInScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exitBtn)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel usernameWrap;
+        private System.Windows.Forms.Button Ans1_Btn;
+        private System.Windows.Forms.PictureBox exitBtn;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Button Ans4_Btn;
+        private System.Windows.Forms.Button Ans3_Btn;
+        private System.Windows.Forms.Button Ans2_Btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label room_info;
+    }
+}
+
---
+++ Our_Client/Trivia_Client/Trivia_Client/client_game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Drawing;
+
+namespace Client_game
+{
+    public partial class Client_game : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        TcpClient client;
+        IPEndPoint serverEndPoint;
+        NetworkStream clientStream;
+
+        public Client_game(TcpClient client, IPEndPoint serverEndPoint, NetworkStream clientStream) {
+            this.client = client;
+            this.serverEndPoint = serverEndPoint;
+            this.clientStream = clientStream;
+
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 6, 6));
+
+            usernameWrap.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, usernameWrap.Width, usernameWrap.Height, 5, 5));
+            Ans1_Btn.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Ans1_Btn.Width + 1, Ans1_Btn.Height + 1, 5, 5));
+
+            try {
+                client = new TcpClient();
+                serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8820);
+                client.Connect(serverEndPoint);
+                clientStream = client.GetStream();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+            panel1.BringToFront();
+
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void exitBtn_MouseHover(object sender, EventArgs e)
+        {
+            exitBtn.BackgroundImage = Trivia_Client.Properties.Resources.exitButtonHover;
+        }
+
+        private void exitBtn_MouseLeave(object sender, EventArgs e)
+        {
+            exitBtn.BackgroundImage = Trivia_Client.Properties.Resources.exitButton;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LogInScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // Window drag
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wparam, int lparam);
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Y <= 50)
+            {
+                Capture = false;
+                SendMessage(Handle, 0x00A1, 2, 0);
+            }
+        }
+
+        private void SignupBtn_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
